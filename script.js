70d91aa058e46a030342b9e4148b113848ecffd7
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-09-02 02:04:46 +0300

    fetched on black merchandise

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let login = document.querySelector();
-login.onclick = function(){
-    button.classlist.toggle();
-};
+fetch(" http://localhost:3000/Tshirts")
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    names(data)
+
+})
+
+
+    function names(data){
+        let descriptionBlack = [];
+      const filmDescription = document.getElementById('ama-black') 
+       for(let decrpt of data){
+        descriptionBlack.push(decrpt.name)
+        let rem = document.getElementById('bttn');
+rem.addEventListener('click', () =>{                  
+       filmDescription.innerHTML = descriptionBlack[0]
+      })
+    }
+ }
+
+
+
