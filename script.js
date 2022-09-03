910006b6af256c5d691f9fbc981b5e557d4f4e4e
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-09-03 03:44:26 +0300

    completed assignment

--- script.js
+++ script.js
@@ -21,41 +21,42 @@ let arrayPrice = []
 fetch(" http://localhost:3000/Tshirts")
 .then(response => response.json())
 .then(data => {
-    console.log(data)
-    names(data)
+    console.log(data)
+    names(data)
 
 })
-    function names(data){
-     for(let tshirt of data){
-      arrayName.push(tshirt.name)
-      arrayPrice.push(tshirt.price)
-      console.log(arrayPrice)
-      console.log(arrayName)
-      let btn = document.getElementById('view')
-      btn.addEventListener('click', ()=>{
-        tName1.innerHTML = arrayName[0]
-        tName2.innerHTML = arrayName[1]
-        tName3.innerHTML = arrayName[2]
-        tName4.innerHTML = arrayName[3]
-        tName5.innerHTML = arrayName[4]
-        tName6.innerHTML = arrayName[5]
-        tName7.innerHTML = arrayName[6]
-        tName8.innerHTML = arrayName[7]
+    function names(data){
+    for(let tshirt of data){
+    arrayName.push(tshirt.name)
+    arrayPrice.push(tshirt.price)
+    console.log(arrayPrice)
+    console.log(arrayName)
+    let btn = document.getElementById('view')
+    btn.addEventListener('click', ()=>{
+        tName1.innerHTML = arrayName[0]
+        tName2.innerHTML = arrayName[1]
+        tName3.innerHTML = arrayName[2]
+        tName4.innerHTML = arrayName[3]
+        tName5.innerHTML = arrayName[4]
+        tName6.innerHTML = arrayName[5]
+        tName7.innerHTML = arrayName[6]
+        tName8.innerHTML = arrayName[7]
 
-        tPrice1.innerHTML = arrayPrice[0]
-        tPrice2.innerHTML = arrayPrice[1]
-        tPrice3.innerHTML = arrayPrice[2]
-        tPrice4.innerHTML = arrayPrice[3]
-        tPrice5.innerHTML = arrayPrice[4]
-        tPrice6.innerHTML = arrayPrice[6]
-        tPrice7.innerHTML = arrayPrice[6]
-        tPrice8.innerHTML = arrayPrice[7]
-      })
+        tPrice1.innerHTML = arrayPrice[0]
+        tPrice2.innerHTML = arrayPrice[1]
+        tPrice3.innerHTML = arrayPrice[2]
+        tPrice4.innerHTML = arrayPrice[3]
+        tPrice5.innerHTML = arrayPrice[4]
+        tPrice6.innerHTML = arrayPrice[6]
+        tPrice7.innerHTML = arrayPrice[6]
+        tPrice8.innerHTML = arrayPrice[7]
+    })
 
-      
-      
-      
-    
-     }
- }
+    
+
+    
+    
+    }
+    }
+
 
